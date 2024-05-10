931a75ee810b9606681f8134576a42313330daee
Prabesh Paswan <pasaprobes@gmail.com>
2024-05-10 13:19:36 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const menu = document.querySelector('nav ul');
+        const menuButton = document.querySelector('.menu-button');
+
+        menuButton.addEventListener('click', function() {
+            menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+</script>
