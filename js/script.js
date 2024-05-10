440941999045ad2fa3ec26cc8273afaa1b904fa7
Prabesh Paswan <pasaprobes@gmail.com>
2024-05-10 13:49:06 +0545

    modified title

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-<script>
+{/* <script>
     document.addEventListener('DOMContentLoaded', function() {
         const menu = document.querySelector('nav ul');
         const menuButton = document.querySelector('.menu-button');
 
         menuButton.addEventListener('click', function() {
-            menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-        });
+            menu.style.display = menu.style.display === 'block' ? 'none' : 'block'}
+        );
     });
-</script>
+</script> */}
